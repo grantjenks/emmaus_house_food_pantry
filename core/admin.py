fe887fef98ccf85b4050c923d65235d6c8e257fe
Grant Jenks <grant.jenks@gmail.com>
2012-07-28 22:25:13 -0400

    Add admin support for BagCount and Setting tables.

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Item, Label, Category, Subcategory
+from models import Item, Label, Category, Subcategory, BagCount, Setting
 
 class ItemAdmin(admin.ModelAdmin):
     pass
@@ -16,3 +16,11 @@ admin.site.register(Category, CategoryAdmin)
 class SubcategoryAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Subcategory, SubcategoryAdmin)
+
+class BagCountAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(BagCount, BagCountAdmin)
+
+class SettingAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Setting, SettingAdmin)
